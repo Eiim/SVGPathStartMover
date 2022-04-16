9cfc5c2d7e3a298599769bb23442f6184ed68cbc
Ethan Chapman <Eiim@users.noreply.github.com>
2022-04-15 23:40:47 -0400

    Fix issues with h/v commands

--- main.js
+++ main.js
@@ -7,26 +7,23 @@ function renderSVG() {
 	var pathData = new SVGPathData(svgText);
 	var absolutePath = pathData.toAbs();
 	
-	console.log(pathData);
-	console.log(absolutePath);
-	
 	var ptsgrp = document.getElementById("pointsGroup");
 	while(ptsgrp.firstChild) {
 		ptsgrp.removeChild(ptsgrp.firstChild);
 	}
 	
 	// Calculate viewport
-	var minx = absolutePath.commands[0].x;
-	var maxx = absolutePath.commands[0].x;
-	var miny = absolutePath.commands[0].y;
-	var maxy = absolutePath.commands[0].y;
+	var minx = absolutePath.commands[0].x ?? 9e999;
+	var maxx = absolutePath.commands[0].x ?? -9e999;
+	var miny = absolutePath.commands[0].y ?? 9e999;
+	var maxy = absolutePath.commands[0].y ?? -9e999;
 	
 	for(var cmd of absolutePath.commands) {
 		if(cmd.type != 1) {			
-			minx = Math.min(cmd.x, minx);
-			miny = Math.min(cmd.y, miny);
-			maxx = Math.max(cmd.x, maxx);
-			maxy = Math.max(cmd.y, maxy);
+			minx = Math.min(cmd.x ?? minx, minx);
+			miny = Math.min(cmd.y ?? miny, miny);
+			maxx = Math.max(cmd.x ?? maxx, maxx);
+			maxy = Math.max(cmd.y ?? maxy, maxy);
 		}
 	}
 	
@@ -37,8 +34,6 @@ function renderSVG() {
 	var asp = previewElt.clientWidth/previewElt.clientHeight;
 	var svgasp = width/height;
 	
-	console.log(asp);
-	
 	// Update viewport
 	width = width * 1.1 * Math.max(svgasp/asp, 1);
 	height = width * 1.1 * Math.max(asp/svgasp, 1);
@@ -47,14 +42,16 @@ function renderSVG() {
 	
 	const scaleFactor = .004*width;
 	
+	var lastx = 0;
+	var lasty = 0;
 	// Add points
 	for(var cmdidx in absolutePath.commands) {
 		var cmd = absolutePath.commands[cmdidx];
 		if(cmd.type != 1) {
 			var pt = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 			pt.setAttribute("r", 3*scaleFactor);
-			pt.setAttribute("cx", cmd.x);
-			pt.setAttribute("cy", cmd.y);
+			pt.setAttribute("cx", cmd.x ?? lastx);
+			pt.setAttribute("cy", cmd.y ?? lasty);
 			pt.setAttribute("class", "point");
 			pt.idx = cmdidx;
 			pt.addEventListener("click", setStart);
@@ -64,6 +61,9 @@ function renderSVG() {
 			miny = Math.min(cmd.y, miny);
 			maxx = Math.max(cmd.x, maxx);
 			maxy = Math.max(cmd.y, maxy);
+			
+			lastx = cmd.x ?? lastx;
+			lasty = cmd.y ?? lasty;
 		}
 	}
 	
@@ -78,6 +78,13 @@ function renderSVG() {
 
 function setStart(e) {
 	console.log(e.currentTarget.idx);
+	
+	var svgText = document.getElementById("pathText").value;
+	var pathData = new SVGPathData(svgText);
+	
+	// 1. Recalculate m/M
+	// 2. If we end with a z/Z, change to an l/L
+	// 3. Reorder commands: Keep index 0, but move next n to end
 }
 
 document.addEventListener("DOMContentLoaded", e => {
